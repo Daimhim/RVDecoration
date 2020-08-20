4cdca5f9bb437936add31e970a46a37b810c3026
Daimhim <daimhim@163.com>
2020-08-20 23:44:03 +0800

    大迭代同步

--- rvdecoration/src/main/java/rv/daimhim/rvdecoration/decoration/base/CreateDecoration.kt
+++ rvdecoration/src/main/java/rv/daimhim/rvdecoration/decoration/base/CreateDecoration.kt
@@ -3,6 +3,6 @@ package rv.daimhim.rvdecoration.decoration.base
 import rv.daimhim.rvdecoration.DecorationBuilder
 import rv.daimhim.rvdecoration.RecycleDecoration
 
-open interface CreateDecoration {
+interface CreateDecoration {
     fun createDecoration(pLRecycleDecoration: RecycleDecoration, p: DecorationBuilder.DecorationParams)
 }
